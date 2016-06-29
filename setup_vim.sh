eaeaf33047c100313089e4f2b3f9cf850ddcc9ea
hell03610 <eraseunavez@gmail.com>
2016-06-29 17:03:16 +0000

    Update

---
+++ setup_vim.sh
@@ -0,0 +1,17 @@
+cp _vimrc ~/.vimrc
+
+cd ~/.vim/bundle
+
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/dsawardekar/portkey.git
+git clone https://github.com/ntpeters/vim-better-whitespace
+git clone https://github.com/nathanaelkane/vim-indent-guides
+
+git clone https://github.com/jelera/vim-javascript-syntax.git
+git clone https://github.com/hoopes/JavaScript-Indent.git
+cd Javascript-Indent
+git checkout no-echo
+cd ..
+
+git clone https://github.com/hell03610/vim-evolution.git
+git clone https://github.com/hell03610/vim-ember-hbs.git
