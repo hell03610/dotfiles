b1b4ee48a3c94ff8bafd35e7fe452a3f9e2850ab
hell03610 <eraseunavez@gmail.com>
2016-07-19 10:37:48 +0200

    add bbye

--- setup_vim.sh
+++ setup_vim.sh
@@ -7,6 +7,7 @@ git clone https://github.com/dsawardekar/portkey.git
 git clone https://github.com/ntpeters/vim-better-whitespace
 git clone https://github.com/nathanaelkane/vim-indent-guides
 git clone https://github.com/bling/vim-bufferline
+git clone https://github.com/moll/vim-bbye.git
 
 git clone https://github.com/jelera/vim-javascript-syntax.git
 git clone https://github.com/hoopes/JavaScript-Indent.git
